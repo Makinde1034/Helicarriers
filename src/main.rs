a3c220c80c3025f28b13384ef8d0f0d46c197c77
Makinde1034 <makinde1034@gmail.com>
2021-10-30 00:28:15 +0100

    Project setup

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+// The modular multiplicative inverse of an interge a, is another integer x such that the product of a and x is congruence to 1 with respect to modulus m. 
+// Two integers a and b are cruguent [ a===b (modulus m). ] when a - b is didvisible by n diving a or b by the modulus have thesame remainder. 
+
+
+fn main(){
+    println!("Hello worldp")
+}
+
+// Function declaration which takes in two parameters of type isize (types whose size depends on the size of the pointer of the underlined machine).
+fn modular_inverse(a : isize, modulus : isize){
+ 
+}
